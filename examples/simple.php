60a80a04de0b68a83be9f7b2ea25f34bc50491d7
Ujjwal Ojha <bring2uo@gmail.com>
2014-03-13 21:47:56 +0545

    Added cache adapter, rate provider

--- examples/simple.php
+++ examples/simple.php
@@ -1,15 +1,17 @@
 <?php
 
 use CurrencyConverter\CurrencyConverter;
+use CurrencyConverter\Cache\Adapter\FileSystem;
     
 chdir(dirname(__DIR__));
 
 require 'vendor/autoload.php';
 
 $converter = new CurrencyConverter;
-$converter->setCachable(true);
-$converter->setCacheDirectory(__DIR__ . '/cache/');
-$converter->setCacheTimeOut(100);
+$converter->setCachable();
+$cacheAdapter = new FileSystem(__DIR__ . '/cache/');
+$cacheAdapter->setCacheTimeout(10);
+$converter->setCacheAdapter($cacheAdapter);
 $amount =  $converter->convert(array('country' => 'US'), array('country' => 'NP'));
 
 echo $amount;
---
+++ src/CurrencyConverter/Cache/Adapter/AbstractAdapter.php
@@ -0,0 +1,51 @@
+<?php
+namespace CurrencyConverter\Cache\Adapter;
+
+abstract class AbstractAdapter implements CacheAdapterInterface
+{
+	/**
+	 * Length of cache time validity in seconds
+     *
+     * @var float
+	 */
+	protected $cacheTimeout = 18000; // 5 hours    
+
+    /**
+     * {@inheritDoc)
+     */
+    public function setCacheTimeOut($cacheTimeout)
+    {
+        $this->cacheTimeout = $cacheTimeout;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritDoc)
+     */
+    public function getCacheTimeOut()
+    {
+        return $this->cacheTimeout;
+    }
+
+    /**
+     * Checks if cache is expired
+     *
+     * @param string $fromCurrency
+     * @param string $toCurrency
+     * @return bool
+     */
+    protected function isCacheExpired($fromCurrency, $toCurrency)
+    {
+        return (time() - $this->getCacheCreationTime($fromCurrency, $toCurrency)) > $this->getCacheTimeOut();
+    }
+
+    /**
+     * Returns timestamp in which cache was created
+     *
+     * @param string $fromCurrency
+     * @param string $toCurrency
+     * @return int
+     */
+    abstract protected function getCacheCreationTime($fromCurrency, $toCurrency);
+}
