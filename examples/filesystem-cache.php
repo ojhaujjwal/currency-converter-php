980df9888f82c40a2cb5a825e7f26453ed38d08e
Ujjwal Ojha <bring2uo@gmail.com>
2014-04-30 09:00:19 +0545

    Simplified examples

--- examples/filesystem-cache.php
+++ examples/filesystem-cache.php
@@ -1,12 +1,8 @@
 <?php
-
-use CurrencyConverter\CurrencyConverter;
-use CurrencyConverter\Cache\Adapter\FileSystem;
-
 require '../vendor/autoload.php';
 
-$converter = new CurrencyConverter;
-$cacheAdapter = new FileSystem(__DIR__ . '/cache/');
+$converter = new CurrencyConverter\CurrencyConverter;
+$cacheAdapter = new CurrencyConverter\Cache\Adapter\FileSystem(__DIR__ . '/cache/');
 $cacheAdapter->setCacheTimeout(DateInterval::createFromDateString('10 second'));
 $converter->setCacheAdapter($cacheAdapter);
 echo $converter->convert('USD', 'NPR');
--- examples/simple.php
+++ examples/simple.php
@@ -1,10 +1,6 @@
 <?php
-
-use CurrencyConverter\CurrencyConverter;
-use CurrencyConverter\Cache\Adapter\FileSystem;
-
 require '../vendor/autoload.php';
 
-$converter = new CurrencyConverter;
+$converter = new CurrencyConverter\CurrencyConverter;
 echo  $converter->convert('USD', 'NPR');
 // or you can do $amount =  $converter->convert(array('country' => 'US'), array('country' => 'NP'));
