9054b0f771dedfd23840417a0a7dc84f356e5aa0
slavielle <sylvain.lavielle@netelios.fr>
2017-11-29 11:21:50 +0100

    issue #16 : Remove unnecessary  comment

--- src/Exception/UnsupportedCurrencyException.php
+++ src/Exception/UnsupportedCurrencyException.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: slavielle
- * Date: 29/11/17
- * Time: 09:30
- */
 
 namespace CurrencyConverter\Exception;
-
 
 class UnsupportedCurrencyException extends InvalidArgumentException implements ExceptionInterface
 {
