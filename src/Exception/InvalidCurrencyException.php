34ce3107313fd2a74edceb145f9313c3721a86f8
slavielle <sylvain.lavielle@netelios.fr>
2017-11-29 10:21:01 +0100

    fix issue #16 : Better Exception on FixerApi.php provider

---
+++ src/Exception/InvalidCurrencyException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slavielle
+ * Date: 29/11/17
+ * Time: 09:30
+ */
+
+namespace CurrencyConverter\Exception;
+
+
+class InvalidCurrencyException extends InvalidArgumentException implements ExceptionInterface
+{
+}
--- tests/CurrencyConverterTest/Provider/FixerApiTest.php
+++ tests/CurrencyConverterTest/Provider/FixerApiTest.php
@@ -14,7 +14,7 @@ class FixerApiTest extends \PHPUnit_Framework_TestCase
             ->expects($this->any())
             ->method('getBody')
             ->will($this->returnValue(
-                new Stream(fopen('data://text/plain,{"base":"EUR","date":"2017-11-02","rates":{"USD":1.1645}}','r'))
+                new Stream(fopen('data://text/plain,{"base":"EUR","date":"2017-11-02","rates":{"USD":1.1645}}', 'r'))
             ));
         $httpClient = $this->getMock(Client::class);
         $httpClient
@@ -30,4 +30,34 @@ class FixerApiTest extends \PHPUnit_Framework_TestCase
             (new FixerApi($httpClient))->getRate('EUR', 'USD')
         );
     }
+
+    public function testGetUnavailableRate()
+    {
+        $response = $this->getMock(ResponseInterface::class);
+        $response
+            ->expects($this->any())
+            ->method('getBody')
+            ->will($this->returnValue(
+                new Stream(fopen('data://text/plain,{"base":"EUR","date":"2017-11-28","rates":{}}', 'r'))
+            ));
+        $httpClient = $this->getMock(Client::class);
+        $httpClient
+            ->expects($this->once())
+            ->method('__call')
+            ->with(
+                'get',
+                [FixerApi::FIXER_API_BASEPATH . '?symbols=XXX&base=EUR']
+            )
+            ->will($this->returnValue($response));
+
+        $expectedExceptionThrown = FALSE;
+        try {
+            (new FixerApi($httpClient))->getRate('EUR', 'XXX');
+        } catch (\Exception $e) {
+            $expectedExceptionThrown = TRUE;
+        }
+        $this->assertTrue($expectedExceptionThrown);
+    }
+
+
 }
