cd42f9b5c92c7b5d84323382b81eae05716b37b0
Johannes <1258430+jbrinksmeier@users.noreply.github.com>
2018-06-04 18:14:54 +0200

    fixes #22 adopt to new endpoint and account structure of fixer.io

--- src/Provider/FixerApi.php
+++ src/Provider/FixerApi.php
@@ -16,7 +16,14 @@ class FixerApi implements ProviderInterface
      *
      * @var string
      */
-    const FIXER_API_BASEPATH = 'https://api.fixer.io/latest';
+    const FIXER_API_BASEPATH = 'data.fixer.io/api/latest';
+
+    /**
+     * The fixer access key
+     *
+     * @var string
+     */
+    protected $accessKey;
 
     /**
      * @var Client
@@ -24,12 +31,22 @@ class FixerApi implements ProviderInterface
     protected $httpClient;
 
     /**
-     * FixerApi constructor.
-     * @param Client|null $httpClient
+     * Flag to switch http(s) usage. required as fixer.io enables https api endpoints for paid accounts only
+     * @var bool
      */
-    public function __construct(Client $httpClient = null)
+    protected $useHttps = false;
+
+    /**
+     * FixerApi constructor.
+     * @param string $accessKey
+     * @param Client|null $httpClient
+     * @param bool $useHttps defaults to false
+     */
+    public function __construct($accessKey, Client $httpClient = null, $useHttps = false)
     {
+        $this->accessKey = $accessKey;
         $this->httpClient = $httpClient ?: new Client();
+        $this->useHttps = (bool)$useHttps;
     }
 
     /**
@@ -38,7 +55,9 @@ class FixerApi implements ProviderInterface
     public function getRate($fromCurrency, $toCurrency)
     {
         $path = sprintf(
-            self::FIXER_API_BASEPATH . '?symbols=%s&base=%s',
+            '%s' . self::FIXER_API_BASEPATH . '?access_key=%s&symbols=%s&base=%s',
+            ($this->useHttps) ? 'https://' : 'http://',
+            $this->accessKey,
             $toCurrency,
             $fromCurrency
         );
--- tests/CurrencyConverterTest/Provider/FixerApiTest.php
+++ tests/CurrencyConverterTest/Provider/FixerApiTest.php
@@ -24,12 +24,12 @@ class FixerApiTest extends \PHPUnit_Framework_TestCase
             ->method('__call')
             ->with(
                 'get',
-                [FixerApi::FIXER_API_BASEPATH . '?symbols=USD&base=EUR']
+                ['http://' . FixerApi::FIXER_API_BASEPATH . '?access_key=SOME-ACCESS-KEY&symbols=USD&base=EUR']
             )
             ->will($this->returnValue($response));
         $this->assertEquals(
             1.1645,
-            (new FixerApi($httpClient))->getRate('EUR', 'USD')
+            (new FixerApi('SOME-ACCESS-KEY', $httpClient))->getRate('EUR', 'USD')
         );
     }
 
@@ -48,12 +48,36 @@ class FixerApiTest extends \PHPUnit_Framework_TestCase
             ->method('__call')
             ->with(
                 'get',
-                [FixerApi::FIXER_API_BASEPATH . '?symbols=XXX&base=EUR']
+                ['http://' . FixerApi::FIXER_API_BASEPATH . '?access_key=SOME-ACCESS-KEY&symbols=XXX&base=EUR']
             )
             ->will($this->returnValue($response));
 
         $this->setExpectedException(UnsupportedCurrencyException::class);
-        (new FixerApi($httpClient))->getRate('EUR', 'XXX');
+        (new FixerApi('SOME-ACCESS-KEY', $httpClient))->getRate('EUR', 'XXX');
+    }
+
+    public function testHttpsUsage()
+    {
+        $response = $this->getMock(ResponseInterface::class);
+        $response
+            ->expects($this->any())
+            ->method('getBody')
+            ->will($this->returnValue(
+                new Stream(fopen('data://text/plain,{"base":"EUR","date":"2017-11-02","rates":{"USD":1.1645}}', 'r'))
+            ));
+        $httpClient = $this->getMock(Client::class);
+        $httpClient
+            ->expects($this->once())
+            ->method('__call')
+            ->with(
+                'get',
+                ['https://' . FixerApi::FIXER_API_BASEPATH . '?access_key=SOME-ACCESS-KEY&symbols=USD&base=EUR']
+            )
+            ->will($this->returnValue($response));
+        $this->assertEquals(
+            1.1645,
+            (new FixerApi('SOME-ACCESS-KEY', $httpClient, true))->getRate('EUR', 'USD')
+        );
     }
     
 }
